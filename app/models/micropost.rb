1368f4904da1c856b1775891cd7f081df1fcbc33
Yuze Chen <yzchen1994@hotmail.com>
2016-02-25 21:36:00 -0500

    Change the maximum length of content

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
 class Micropost < ActiveRecord::Base
-  validates :content, length: {maximum: 10} #Set max content input 10
+  validates :content, length: {maximum: 140} #Set max content input 10
   belongs_to :user
 end
