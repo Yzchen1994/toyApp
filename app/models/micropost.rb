2291cccf93558de0b19bf74942749eab5b0d4f98
Yuze Chen <yzchen1994@hotmail.com>
2016-02-25 21:40:00 -0500

    Add a check to make sure the content is not empty.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
-  validates :content, length: {maximum: 140} #Set max content input 10
+  validates :content, length: {maximum: 140}, #Set max content input 140
+              presence: true #Content shall not be empty.
   belongs_to :user
 end
